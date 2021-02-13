ae23c356d74d19db8cfa07befa8f9ab6e90574b2
tanveerhasan <tanveer.cse01@gmail.com>
2021-02-13 16:06:25 +0600

    add custom navigation drawer

--- app/src/main/java/com/example/ramadan/MainActivity.java
+++ app/src/main/java/com/example/ramadan/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.ramadan;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.ramadan.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ActivityMainBinding binding;
+    Home homeFragment;
+    int valu =0;
+    Toolbar toolbarr;
+    TextView toolbarTitle;
+
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        toolbarr=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbarr);
+
+        toolbarTitle=findViewById(R.id.toolbarTitle);
+        toolbarTitle.setText(R.string.app_name);
+
+        setSupportActionBar(binding.include.toolbar);
+
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this,
+                binding.drawerLayout,binding.include.toolbar,R.string.open, R.string.close);
+        actionBarDrawerToggle.getDrawerArrowDrawable().setColor(getColor(R.color.white));
+
+
+        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        binding.navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
